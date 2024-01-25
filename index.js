27ce358a54d5c8c7697fea1295a0acf64b3c8c09
Sandra Felicio <mjpfelicia@gmail.com>
2024-01-25 12:17:36 -0300

    -criando pagina html e text txt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
-const portar = 443;
+const readline = require("readline");
+const portar = 8080;
 const servidor = http.createServer((req, res) => {
   fs.readFile("pagina.html", (err, data) => {
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -23,4 +24,15 @@ async function readFileByLine(file) {
   }
 }
 
-//readFileByLine('tex.txt')
+
+function criarArquivo(nomeDoArquivo) {
+  fs.appendFileSync(nomeDoArquivo, `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum sodales ullamcorper. Nullam
+  vulputate ligula  et suscipit egestas.
+  Fusce mi turpis, sagittis quis molestie sed, interdum sed enim. Nullam metus turpis, tempor
+  vel.`, (err) => {
+    if (err) throw err;
+  });
+}
+criarArquivo('text.txt')
+readFileByLine('text.txt')
